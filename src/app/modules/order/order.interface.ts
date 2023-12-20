b89085aa76377991981fcec7bf405a3385d385a6
atique-atq <atiqur.rahman@bevycommerce.com>
2023-12-20 20:41:22 +0600

    first commit

--- src/app/modules/order/order.interface.ts
+++
@@ -1,7 +0,0 @@
-import { Model } from 'mongoose'
-
-export type TOrder = {
-  productName: string
-  price: number
-  quantity: number
-}
--- src/app/modules/order/order.validation.ts
+++
@@ -1,9 +0,0 @@
-// user.validation.ts
-
-import { z } from 'zod'
-
-export const orderSchema = z.object({
-  productName: z.string({ required_error: 'Product Name is required' }),
-  price: z.number({ required_error: 'Price is required' }),
-  quantity: z.number({ required_error: 'Quantity is required' }),
-})
